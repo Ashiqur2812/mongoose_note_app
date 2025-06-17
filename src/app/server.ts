a3ac7fcaf994644aaec98fe5f74a323f725892f8
Ashiqur <ashiqur2812@gmail.com>
2025-06-17 10:54:49 +0600

    setting up mongoose in note app project

---
+++ src/app/server.ts
@@ -0,0 +1,21 @@
+import { Server } from 'http';
+import app from './app';
+import mongoose from 'mongoose';
+
+let server: Server;
+
+const PORT = 4000;
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb+srv://todoapp:todoapp@cluster0.yt5iw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        console.log('Connected to mongoDB using mongoose')
+        server = app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
